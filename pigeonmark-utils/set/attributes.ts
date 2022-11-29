cc570d1f0ac4c6764d151b21f68989ddfdbb61f8
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-11-30 00:51:13 +1100

    pigeonmark-utils@2.1.2 - Solve some svelte-check fails due to useless imports

--- pigeonmark-utils/set/attributes.ts
+++ pigeonmark-utils/set/attributes.ts
@@ -1,11 +1,5 @@
 import type { PMTag, PMXMLPI, PMAttributes } from '../types'
 import { isProcessingInstruction, isTagWithAttributes, isTagWithoutAttributes } from '../get/type.js'
-import getChildNodes from '../get/child-nodes.js'
-import getAttributes from '../get/attributes.js'
-import getName from '../get/name.js'
-import createTag from '../create/tag.js'
-import createXMLPI from '../create/xmlpi.js'
-
 
 /**
  * sets all the attributes of a tag or xmlpi, or an empty object if it doesn't have any
